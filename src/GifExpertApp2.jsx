c7b95a92da320a774ebc8a0559902b70b41834ad
Sebastian L Moran <sbssing7@gmail.com>
2023-10-09 09:55:08 -0500

    Agregar una nueva categoria (no dinámica) de GIFS al listado

--- src/GifExpertApp2.jsx
+++ src/GifExpertApp2.jsx
@@ -5,11 +5,17 @@ export const GifExpertApp2 = () => {
   
   const [categories, setCategories] = useState(['Metallica','Megadeth']);
   
+  const onAddCategory = () => {
+    setCategories(['Slayer',...categories])
+  };
+
   return (
 
     <> 
 
     <h1>GifExpertApp2</h1>
+
+    
 
     <ol>
       {
@@ -20,6 +26,11 @@ export const GifExpertApp2 = () => {
           })
       }
     </ol>
+
+    <button onClick={onAddCategory}>
+      Agregar Categoria
+    </button>
+    
 
     </>
     
