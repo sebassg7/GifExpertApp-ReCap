24a2881b659490668e35b962e997b85ea9bc0e54
Sebastian L Moran <sbssing7@gmail.com>
2023-10-09 09:33:36 -0500

    Creación de los archivos/Componente GifExpertApp

---
+++ src/GifExpertAdd2.jsx
@@ -0,0 +1,5 @@
+export const GifExpertAdd2 = () => {
+  return (
+    <h1>GifExpertAdd2</h1>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { GifExpertAdd2 } from './GifExpertAdd2.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GifExpertAdd2 />
+  </React.StrictMode>,
+)
