9f1c84770f37f5784710fef8f8e9d95b08cfacd7
Sebastian L Moran <sbssing7@gmail.com>
2023-10-09 09:48:14 -0500

    Creación de una lista de categorias de GIFS

--- src/GifExpertApp2.jsx
+++ src/GifExpertApp2.jsx
@@ -1,6 +1,28 @@
+import { useState } from "react";
+
+useState
 export const GifExpertApp2 = () => {
+  
+  const [categories, setCategories] = useState(['Metallica','Megadeth']);
+  
   return (
+
+    <> 
+
     <h1>GifExpertApp2</h1>
+
+    <ol>
+      {
+          categories.map(category => {
+            return(
+              <li key={category}>{category}</li>
+            )
+          })
+      }
+    </ol>
+
+    </>
+    
   )
 };
 
